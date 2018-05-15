64c743878bd406b336d4327394d1856492ac167e
erikjearl <league@iMac-2.attlocal.net>
2018-05-15 16:56:25 -0700

    drum kit, photo quiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,33 +22,63 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+                int counter = 0 ;
 		// 2. create a variable of type "Component" that will hold your image
-
+          Component comp;
 		// 3. use the "createImage()" method below to initialize your Component
-
+          comp = createImage("https://upload.wikimedia.org/wikipedia/commons/f/ff/Domestic_goat_kid_in_capeweed.jpg");
 		// 4. add the image to the quiz window
-
+          quizWindow.add(comp);
 		// 5. call the pack() method on the quiz window
-
-		// 6. ask a question that relates to the image
-
+          quizWindow.pack();
+		
+          
+          
+          // 6. ask a question that relates to the image
+          String q1 = JOptionPane.showInputDialog("is the picture a goat");
 		// 7. print "CORRECT" if the user gave the right answer
-
+          
+          if (q1.equals("yes")) {
+          System.out.println("CORRECT");
+          JOptionPane.showMessageDialog(null, "CORRECT");
+          counter++;
+          }
 		// 8. print "INCORRECT" if the answer is wrong
-
+          else {
+          System.out.println("INCORRECT");
+          JOptionPane.showMessageDialog(null, "INCORRECT");
+          }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+         
+          quizWindow.remove(comp);
 		// 10. find another image and create it (might take more than one line of code)
-
+          Component pic2 = createImage("https://img.huffingtonpost.com/asset/581c1eab150000b700531aa2.jpeg?cache=u6ye1hoy5s&ops=1910_1000");
 		// 11. add the second image to the quiz window
-
+          quizWindow.add(pic2);
 		// 12. pack the quiz window
-
+          quizWindow.pack();
 		// 13. ask another question
-
-		// 14+ check answer, say if correct or incorrect, etc.
-
+          String q2 = JOptionPane.showInputDialog("is the picture not a goat");
+          // 14+ check answer, say if correct or incorrect, etc.
+          if (q2.equals("yes")) {
+        	  System.out.println("CORRECT");
+        	  JOptionPane.showMessageDialog(null, "CORRECT");
+        	  counter++;
+              }
+          else if (q2.equals("extra credit?"))
+          {
+        	  counter+=99;
+          }
+          
+          else {
+        	  System.out.println("INCORRECT");
+        	  JOptionPane.showMessageDialog(null, "INCORRECT");
+          }
+          
+          JOptionPane.showMessageDialog(null, "Your Finial Score was " + counter + " out of 2");
+          double x = (double)counter / 2;
+          
+          JOptionPane.showMessageDialog(null, "That is " + x*100 + "%");
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
@@ -61,6 +91,7 @@ public class PhotoQuiz {
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
 	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	
 }
 
 
